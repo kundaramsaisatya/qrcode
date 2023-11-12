a8901a0c441f165ffdaf517add55f06ad2ff7975
satya <kundaramsaisatya@gmail.com>
2023-11-12 15:44:03 +0530

    Added download option!

--- qrscript.js
+++ qrscript.js
@@ -1,12 +1,27 @@
 
 let qrtext= document.getElementById("qrtext");
 let qrimg= document.getElementById("qrimg");
+let downloadLink = document.getElementById("downloadLink");
 
 function myfunction(){
-    if(qrtext.value.length>0){
+    if(qrtext.value.trim().length>0){
         qrimg.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrtext.value;
 
-        imgbox.classList.add("showimg");
+        imgbox.classList.add("showimg");   
+        
+
+        downloadLink.href = qrimg.src;
+        downloadLink.download = "qrcode.png";
+        
+
+
+        fetch(qrimg.src)
+        .then(response => response.blob())
+        .then(blob => {
+            const blobURL = URL.createObjectURL(blob);
+            downloadLink.href = blobURL;
+        })
+        .catch(error => console.error("Error fetching image:", error));
     }
     else if(qrtext.value.length==0){
         qrimg.src="";
@@ -14,7 +29,18 @@ function myfunction(){
         setTimeout(() => {
             alert("Enter URL");
         }, 300);
+        
     }
+    else if(qrtext.value.length=" "){
+        qrimg.src="";
+        imgbox.classList.remove("showimg");
+        setTimeout(() => {
+            alert("Input Empty!");
+        }, 300);
+
+        
+    }
+
 }
 
 
