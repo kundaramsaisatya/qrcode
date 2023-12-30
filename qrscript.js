fe6edf9a2fbe0acc29c8f1928ada23128fca95c2
Satya <kundaramsatya@gmail.com>
2023-12-30 15:14:10 +0530

    Changed some UI

--- qrscript.js
+++ qrscript.js
@@ -3,14 +3,15 @@ let qrtext= document.getElementById("qrtext");
 let qrimg= document.getElementById("qrimg");
 let downloadLink = document.getElementById("downloadLink");
 
+downloadLink.classList.add("btnremove");
 function myfunction(){
     if(qrtext.value.trim().length>0){
         qrimg.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrtext.value;
-
+        
         imgbox.classList.add("showimg");   
         downloadLink.href = qrimg.src;
         downloadLink.download = "qrcode.png";
-    
+        
         fetch(qrimg.src)
         .then(response => response.blob())
         .then(blob => {
@@ -18,6 +19,8 @@ function myfunction(){
             downloadLink.href = blobURL;
         })
         .catch(error => console.error("Error fetching image:", error));
+        
+        downloadLink.classList.remove("btnremove");
     }
     else if(qrtext.value.length==0){
         qrimg.src="";
